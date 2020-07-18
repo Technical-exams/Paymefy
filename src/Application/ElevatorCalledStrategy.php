f046b8e826fd07e4aad3c22f498456fdc838b3b1
Xavi Subirà <xavi@proweb21.com>
2020-07-19 00:16:47 +0200

    FIX\!\! on ElevatorCalledStrategy getElevator function Function will not work with a domain object such BuildingElevatorState, because doesn't really need. Working with a DTO, or event with a summary bulked as an array is enough for consulting the state. This kind of result is offered by ElevatorStateService getState function (see commit b5c0508ef02c5f18adf0732d713ab210268a28c5)

--- src/Application/ElevatorCalledStrategy.php
+++ src/Application/ElevatorCalledStrategy.php
@@ -1,7 +1,5 @@
 <?php namespace Proweb21\Elevator\Application;
 
-use Proweb21\Elevator\Model\BuildingElevatorsState;
-
 /**
  * Interface for ElevatorCalledStrategy classes
  * 
@@ -16,9 +14,10 @@ interface ElevatorCalledStrategy
     /**
      * Returns the best elevator candidate for a trip starting at calling flat and ending at destination flat
      *
+     * @param array $state The elevators state (as array) to use for applying the strategy and find the right elevator
      * @param integer $calling_flat The flat where the elevator is called
      * @param integer $destination_flat The flat selected as trip destination
      * @return string|null The best building elevator for performing the trip
      */
-    public function getElevator(BuildingElevatorsState $state, int $calling_flat, int $destination_flat) : ?string;
+    public function getElevator(array $state, int $calling_flat, int $destination_flat) : ?string;
 }
