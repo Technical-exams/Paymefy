d69aad13f244c236a2fe0196c23bfcd50da04f17
Xavi Subirà <xavi@proweb21.com>
2020-07-18 19:00:49 +0200

    FIX!! ElevatorRepository => ElevatorCollection [DOMAIN][MODEL]

--- src/Domain/Model/ElevatorCollection.php
+++ src/Domain/Model/ElevatorCollection.php
@@ -1,10 +1,10 @@
 <?php namespace Proweb21\Elevator\Model;
 
 /**
- * Base class for ElevatorsRepositories
+ * Collection of Elevators 
  */
-abstract class AbstractElevatorRepository
-    implements ElevatorRepository
+final class ElevatorCollection
+    
 {
     /**
      * The list of elevators compounding the collection
@@ -14,34 +14,42 @@ abstract class AbstractElevatorRepository
     protected $elevators = [];
 
     /**
-     * {@inheritDoc}
-     *
-     */    
-    public function save(Elevator $elevator): Elevator
+     * Stores an elevator to the collection if not present
+     * 
+     * @param Elevator $elevator
+     * @return Elevator The elevator added
+     */
+    public function add(Elevator $elevator): Elevator
     {
         $this->elevators[$elevator->id] = $elevator;
+        
         return $elevator;
     }
 
     /**
-     * {@inheritDoc}
+     * Removes an elevator from the collection
      *
+     * @param Elevator $elevator The elevator to remove from the collection
+     * @return Elevator The given elevator once removed (in case it was present)
      */
     public function remove(Elevator $elevator) : Elevator
     {
         if (array_key_exists($elevator->id,$this->elevators))
             unset($this->elevators[$elevator->id]);
+
         return $elevator;
     }
 
     /**
-     * {@inheritDoc}
+     * Finds an elevator in the collection given its id
      *
+     * @param string $id
+     * @return Elevator|False The elevator with the given id or null if no elevator was found
      */
     public function findOne(string $id)
     {        
         $result = false;
-        
+
         if (array_key_exists($id,$this->elevators))
             $result = $this->elevators[$id];
 
