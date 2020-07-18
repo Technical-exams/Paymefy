0ed2b28757c491040a6c54aa38d713b018bb4c65
Xavi Subirà <xavi@proweb21.com>
2020-07-18 16:33:28 +0200

    MinutePassed event and SystemTime singleton [EVENTS][TIME]

---
+++ src/Events/SystemTime.php
@@ -0,0 +1,57 @@
+<?php namespace Proweb21\Elevator\Events;
+
+/**
+ * Singleton class intended to provide the current application time
+ * as if it was an actual system clock
+ * 
+ * This class is necessary due time pass simulations
+ */
+final class SystemTime
+    implements EventHandler
+{
+    /**
+     * Which time the system is in currently
+     *
+     * @var \DateTimeImmutable
+     */
+    protected static $time;
+
+    protected static $instance;
+
+    /**
+     * Singleton class instance accessor
+     */
+    public static function instance() : SystemTime
+    {
+        self::$instance = self::$instance ? : new static();
+        return self::$instance;
+    }
+
+    /**
+     * Protected constructor
+     * to protect the singleton
+     */
+    protected function __construct(){      
+    }
+    
+    /**
+     * Handles the pass of time events
+     *
+     * @param MinutePassed $event
+     * @return void
+     */
+    public function handle(MinutePassed $event){
+        self::$time = $event->time;
+    }
+
+    /**
+     * Gets the clock time
+     *
+     * @return \DateTimeImmutable
+     */
+    public function getCurrentTime(): \DateTimeImmutable
+    {
+        return self::$time ? : new \DateTimeImmutable();
+    }
+
+}
