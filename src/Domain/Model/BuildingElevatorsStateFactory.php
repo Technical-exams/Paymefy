c735d4c35996c4447f59ae8113c5892556d496ea
Xavi Subirà <xavi@proweb21.com>
2020-07-18 21:08:07 +0200

    BuildingElevatorsState Factory [DOMAIN][MODEL] This factory gets the buildnig's state inspecting its elevators. This factory must be used by those classes whose methods consume elevators state, such strategies or application services

---
+++ src/Domain/Model/BuildingElevatorsStateFactory.php
@@ -0,0 +1,27 @@
+<?php namespace Proweb21\Elevator\Model;
+
+/**
+ * Factory for creating instances of a BuildingElevatorState
+ * 
+ * Each instance created depends on a building an its Elevators
+ * 
+ */
+class BuildingElevatorsStateFactory
+{
+    /**
+     * Factory method for creating a Building Elevators State
+     *
+     * @param Building $building The Building to use for creating the State
+     * @return BuildingElevatorsState The State created from the building elevators
+     */
+    public function create(Building $building) : BuildingElevatorsState
+    {
+        $result = new BuildingElevatorsState();
+
+        foreach($building->getElevators() as $elevator){
+            $result->setState($elevator);
+        }
+
+        return $result;
+    }
+}
