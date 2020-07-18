f2cb82c3743bd3f39e7fa18c7d2b6163c0e64caa
Xavi Subirà <xavi@proweb21.com>
2020-07-18 14:40:07 +0200

    Observable pattern interfaces [EVENTS][OBSERVABLE]

---
+++ src/Events/Observable.php
@@ -0,0 +1,17 @@
+<?php namespace Proweb21\Elevator\Events;
+
+/**
+ * Interface for classes whose are observed
+ * @see https://en.wikipedia.org/wiki/Observer_pattern
+ */
+interface Observable
+{
+  
+    /**
+     * Publish an event notifying any observers
+     *
+     * @param ObservableEvent $event
+     * @return void
+     */
+    public function publish(ObservableEvent $event);
+}
