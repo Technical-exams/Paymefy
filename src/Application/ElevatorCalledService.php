331f6843032a7dda45018ca978cafb7bc93e68f0
Xavi Subirà <xavi@proweb21.com>
2020-07-19 20:13:25 +0200

    REFACTOR\! on ElevatorCalledService [APP][EVENTS][HANDLER] As stated in several comments the state management responsibility has been delegated to a Domain Service (ElevatorStateService), which observes Elevator flat changes and updates the ElevatorsState. This service is consumed to get the actual Building Elevators State, what is used by the internal Strategy to select the best elevator candidate to move for the received call (see function handle). By elevator movements, the ElevatorStateService will be up to date due its observation of the Elevators in the Model

--- src/Application/ElevatorCalledService.php
+++ src/Application/ElevatorCalledService.php
@@ -2,9 +2,7 @@
 
 use Proweb21\Elevator\Events\EventHandler;
 use Proweb21\Elevator\Model\Building;
-use Proweb21\Elevator\Model\BuildingElevatorsStateFactory as StateFactory;
-use Proweb21\Elevator\Model\BuildingElevatorsState;
-
+use Proweb21\Elevator\Model\ElevatorsStateService as StateService;
 
 /**
  * The handler for attending the ElevatorCalled command events
@@ -29,11 +27,11 @@ class ElevatorCalledService
     protected $building;
 
     /**
-     * The Building Elevators state
+     * The Building Elevators state Service
      *
-     * @var BuildingElevatorsState
+     * @var StateService
      */
-    protected $state;
+    protected $state_service;
 
     /**
      * Service Constructor 
@@ -43,19 +41,14 @@ class ElevatorCalledService
      *
      * @param Building $building The building with Elevators
      * @param ElevatorCalledStrategy $strategy The strategy to apply
-     * @param StateFactory $factory A factory helping the creation of an State of the Building
-     * 
-     * @internal The State should be managed indeed in a Domain Service, which may be Observing the Elevator Entities
+     * @param StateService $service A state management service for the building
      * 
      */
-    public function __construct(Building $building, ElevatorCalledStrategy $strategy, StateFactory $factory)
+    public function __construct(Building $building, ElevatorCalledStrategy $strategy, StateService $service)
     {
-        // TODO: Move the State management to a Domain Service Observing Elevators of each building
-        // That service may offer the state given a building
-
         $this->strategy = $strategy;
         $this->building = $building;
-        $this->state = $factory->create($building);
+        $this->state_service = $service;
     }
 
     /**
@@ -72,18 +65,14 @@ class ElevatorCalledService
         // Does nothing if call comes from the destination flat
         if ($event->calling_flat == $event->destination_flat) return;
 
-        $calling_flat = array_search($event->calling_flat,$this->building->getFlats());
-        $destination_flat = array_search($event->destination_flat,$this->building->getFlats());
-
-        if ($calling_flat === FALSE)
+        if (!in_array($event->calling_flat,$this->building->getFlats()))
             throw new \InvalidArgumentException(sprintf("Bad flat %s, for the elevator call",$event->calling_flat));
-        if ($destination_flat === FALSE)
+        if (!in_array($event->destination_flat, $this->building->getFlats()))
             throw new \InvalidArgumentException(sprintf("Bad flat %s, for the elevator destination",$event->destination_flat));
             
-
         // Looks for the right Elevator using the given strategy
         $elevator_id = $this->strategy->getElevator(
-                        $this->state,
+                        $this->state_service->getState(),
                         $event->calling_flat,
                         $event->destination_flat
         );
@@ -91,11 +80,7 @@ class ElevatorCalledService
         if (!is_null($elevator_id)) {            
             // Elevator is moved twice
             $this->building->moveElevator($elevator_id, $event->calling_flat);
-            $elevator = $this->building->moveElevator($elevator_id, $event->destination_flat);            
-            
-            // State is updated with the elevators new position
-            // TODO: Move this to the model via Elevators Observer service
-            $this->state->setState($elevator);
+            $this->building->moveElevator($elevator_id, $event->destination_flat);                        
         }
     }
 }
