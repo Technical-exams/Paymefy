86cc7bc28257833f46bc48cd47c9f4b437739f68
Xavi Subirà <xavi@proweb21.com>
2020-07-20 00:58:56 +0200

    ElevatorStats & repository [DOMAIN][MODEL]

---
+++ src/Domain/Model/ElevatorStats.php
@@ -0,0 +1,62 @@
+<?php namespace Proweb21\Elevator\Model;
+
+/**
+ * DTO for persisting Elevator Stats taken during application execution
+ */
+final class ElevatorStats
+{
+    /**
+     * Which is the elevator
+     *
+     * @var string
+     */
+    protected $elevator;
+
+    /**
+     * Which is the identifier of the flat on the building
+     * where elevator was when the stat was taken
+     *
+     * @var int
+     */
+    protected $flat_name;
+
+    /**
+     * Which is the flat position in the building
+     *
+     * @var int
+     */
+    protected $flat_no;
+
+    /**
+     * How many flats has moved the elevator during the application execution
+     *
+     * @var int
+     */
+    protected $total_moves;
+
+    /**
+     * How many flats has the elevator moved in its last trip
+     *
+     * @var int
+     */
+    protected $last_movement;
+
+    /**
+     * When the stats were taken
+     *
+     * @var \DateTimeImmutable
+     */
+    protected $last_update;
+
+
+    public function __construct(string $elevator, int $flat_no, int $flat_name, int $moved, int $accumulated, \DateTimeImmutable $last_update)
+    {
+        $this->elevator = $elevator;
+        $this->flat_no = $flat_no;
+        $this->flat_name = $flat_name;
+        $this->total_moves = $accumulated;
+        $this->last_movement = $moved;
+        $this->last_update = $last_update;
+    }
+
+}
---
+++ src/Domain/Model/ElevatorStatsRepository.php
@@ -0,0 +1,30 @@
+<?php namespace Proweb21\Elevator\Model;
+
+/**
+ * Repository used to collect Elevator Stats
+ */
+interface ElevatorStatsRepository
+{
+    /**
+     * Adds ElevatorStats to repository
+     *
+     * @param ElevatorsStats $stats
+     * @return void
+     */
+    public function add(ElevatorStats $stats);
+
+    /** 
+     * Gets the stats of all Elevators 
+     * @oaram bool $summarized()
+     * @return \Traversable
+     */
+    public function findAll(bool $summarized) : \Traversable;
+
+    /**
+     * Gets the last stats of Elevator
+     * 
+     * @return ElevatorStats|null The elevator stats if found or null if no stats in the Repository
+     * 
+     */
+    public function last(string $elevator): ?ElevatorStats;
+}
