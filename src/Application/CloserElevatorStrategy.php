59529cb5cf37108a99e29e2e141f06d5c41a6242
Xavi Subirà <xavi@proweb21.com>
2020-07-20 13:04:21 +0200

    CRITICAL FIX!! on CloseElevatorStrategy [APP][EVENTS][LOGIC] The search for elevators upwards never reached the top flat due a bad if condition, so it falled into an infinite loop

--- src/Application/CloserElevatorStrategy.php
+++ src/Application/CloserElevatorStrategy.php
@@ -17,20 +17,30 @@ class CloserElevatorStrategy
         $calling_flat = $this->getCallingFlatPosition($calling_flat,$flats_state);
         // We must find the closer elevator to calling flat
         $upwards = $calling_flat+1;
+        $ended_upwards = false;
         $downwards = $calling_flat-1;      
+        $ended_downwards = false;
 
         // Look at the calling flat if there is an elevator, that is the candidate
         if (count($flats_state[$calling_flat]->elevators))
             $result = reset($flats_state[$calling_flat]->elevators);
 
-        while ($result === FALSE){
+        while (($result === FALSE) && (! ($ended_downwards && $ended_upwards))){
             // Upwards elevators have preference because of gravity and energy saving lower impact
-            $result = count($flats_state[$upwards]->elevators) ? reset($flats_state[$upwards]->elevators) : FALSE;
-            if ($result === FALSE)
-                $result = count($flats_state[$downwards]->elevators) ? reset($flats_state[$downwards]->elevators) : FALSE;
+            if ($upwards < count($flats_state)) {
+                $result = count($flats_state[$upwards]->elevators) ? reset($flats_state[$upwards]->elevators) : false;
+                $upwards++;
+            }else{
+                $ended_upwards = true;
+            }            
+            if (($result === false) && ($downwards >=0)) {
+                $result = count($flats_state[$downwards]->elevators) ? reset($flats_state[$downwards]->elevators) : false;
+                $downwards--;
+            }else{
+                $ended_downwards = true;
+            }
+
             
-            if ($upwards < count($flats_state)-1) $upwards++;
-            if ($downwards >0) $downwards--;
         }
         return $result;
     }
@@ -51,7 +61,7 @@ class CloserElevatorStrategy
 
         foreach($flats_state as $flat => $flatState)
         {
-            if ($flatState->flat->id === $flat_id){
+            if ($flatState->id === $flat_id){
                 $result = $flat;
                 break;
             }
