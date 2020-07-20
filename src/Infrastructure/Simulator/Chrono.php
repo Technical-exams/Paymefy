8fb5e9cd9e53595caa724b375cf91e5d78ca4dfa
Xavi Subirà <xavi@proweb21.com>
2020-07-20 10:50:41 +0200

    ChronoSimulator and dependencies [INFRA][SIMULATOR][EVENTS][TIME]

---
+++ src/Infrastructure/Simulator/Chrono.php
@@ -0,0 +1,66 @@
+<?php namespace Proweb21\Elevator\Infrastructure\Simulator;
+
+/**
+ * Chrono acts like a chronometer
+ * Counts minutes and accumulated hours
+ * 
+ * The chrono current count can be queried calling "minutes" function
+ */
+class Chrono
+{
+    /**
+     * Which hour has been reached by the chrono
+     *
+     * @var integer
+     */
+    protected $hour = 0;
+
+    /**
+     * Whose minutes have been reached by the chrono
+     *
+     * @var integer
+     */
+    protected $minutes = 0;
+
+    /**
+     * How many minutes to count
+     *
+     * @var integer
+     */
+    protected $count = 60;
+
+    
+    protected $counted= 0;
+
+    public function __construct(int $count = 60, int $hour=null)
+    {
+        $this->reset($hour);
+        $this->count = (!is_null($count) && (0<$count && 60>=$count))?$count:$this->count;
+    }
+
+    public function reset(int $hour=null, int $minutes=0){
+        $this->minutes = $minutes;
+        $this->hour = (!is_null($hour) && (0<=$hour && 24>$hour))?$hour:$this->hour;
+
+    }
+
+    public function minutes()
+    {
+        $count = 0;
+        // We must chrono minutes while count is reached.
+        while ($count < $this->count) {            
+            $count++;
+            $this->minutes++;
+            // When minutes reach 60, minutes and hour are readjusted
+            if ($this->minutes >= 60) {
+                $this->reset(
+                    $this->hour + intval($this->minutes / 60),
+                    $this->minutes % 60
+                );
+            }
+            
+            yield ["hour"=>$this->hour,"minute"=>$this->minutes];
+        }        
+    }
+
+}
---
+++ src/Infrastructure/Simulator/ChronoSimulator.php
@@ -0,0 +1,51 @@
+<?php namespace Proweb21\Elevator\Infrastructure\Simulator;
+
+use Proweb21\Elevator\Events\MinutePassed;
+
+/**
+ * Simulates the pass of a given time expressed in minutes
+ * starting from an hour of the day
+ * 
+ * @uses Chrono
+ * @uses TimeBus
+ */
+class ChronoSimulator
+{
+    /**
+     * Bus where the clock events are notified
+     *
+     * @var TimeBus
+     */
+    protected $bus;
+
+    /**
+     * Clock used to produce clock events
+     *
+     * @var HourClock
+     */
+    protected $clock;
+
+    public function __construct(TimeBus $bus, int $count=null)
+    {
+        $this->bus = $bus;
+        $this->clock = new Chrono($count);
+    }
+
+    /**
+     * Starts the Hour simulator
+     *
+     * @param int $hour
+     * @return void
+     */
+    public function start(int $hour = null)
+    {
+        if (!is_null($hour))
+            $this->clock->reset($hour);     
+        foreach($this->clock->minutes() as $clock_time){            
+            $event = new MinutePassed($clock_time["hour"],$clock_time["minute"]);
+            $this->bus->dispatch($event);
+            print(implode(':',$clock_time)."\n");
+        }
+    }
+
+}
