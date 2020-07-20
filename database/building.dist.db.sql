871255fffa6adadb5dbabfd7a1cafa35083b6b36
Xavi Subirà <xavi@proweb21.com>
2020-07-20 21:57:48 +0200

    MAJOR CHANGES on Database [DB] Database creation script resets database to initial empty tables. A view for querying the elevator stats summary has been created Table movements was unnecessary and has been removed

--- database/building.dist.db.sql
+++ database/building.dist.db.sql
@@ -1,12 +1,6 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS `movements` (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`time`	INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`elevator`	TEXT NOT NULL,
-	`origin`	INTEGER NOT NULL DEFAULT 0,
-	`destination`	INTEGER NOT NULL
-);
-CREATE TABLE IF NOT EXISTS `state_changes` (
+DROP TABLE IF EXISTS `elevator_stats`;
+CREATE TABLE IF NOT EXISTS `elevator_stats` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`time`	INTEGER NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`elevator`	TEXT NOT NULL,
@@ -14,4 +8,18 @@ CREATE TABLE IF NOT EXISTS `state_changes` (
 	`accum_moves`	INTEGER NOT NULL DEFAULT 0,
 	`last_move`	INTEGER NOT NULL DEFAULT 0
 );
+
+DROP VIEW IF EXISTS `elevator_stats_summary`;
+CREATE VIEW IF NOT EXISTS `elevator_stats_summary` (`time`, `elevator`, `stopped_at`, `accum_moves`, `last_move`)
+AS
+SELECT `a`.`time`, `a`.`elevator`, `a`.`stopped_at`, `a`.`accum_moves`, `a`.`last_move`
+FROM `elevator_stats` as `a`
+JOIN (SELECT MAX(accum_moves) as `accum_moves`, `time`, `elevator`
+FROM `elevator_stats`
+GROUP by `time`, `elevator`) as `b`
+ON `a`.`time`= `b`.`time`
+AND `a`.`elevator` = `b`.`elevator`  
+WHERE `a`.`accum_moves` = `b`.`accum_moves`
+ORDER BY `a`.`time`, `a`.`elevator`
+
 COMMIT;
