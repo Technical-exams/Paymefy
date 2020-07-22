fa6884f58c310d76a558b3dd0e249e3726635306
Xavi Subirà <xavi@proweb21.com>
2020-07-22 11:24:07 +0200

    WORKAROUND applied on SimulationController [CONTROLLER][DI][LARAVEL] ElevatorStatsRepository cannot be injected due an unknown reason. Container has been injected instead

--- app/Http/Controllers/SimulationController.php
+++ app/Http/Controllers/SimulationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Facades\AgbarSimulator;
 use Illuminate\Http\Request;
-use Proweb21\Elevator\Model\ElevatorStatsRepository;
+use Psr\Container\ContainerInterface;
 
 class SimulationController extends Controller
 {
@@ -14,11 +14,17 @@ class SimulationController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request, ElevatorStatsRepository $repository)
+    public function __invoke(Request $request, ContainerInterface $container)
     {
+        $repository = $container->make('stats.repository');
+        $repository->removeAll();
         AgbarSimulator::start();
+        //TODO MOVE LOGIC TO STATE SERVICE
+        $building_elevators = AgbarSimulator::getBuilding()->getElevators();
+        $state=[];
+        foreach($building_elevators as $name => $elevator_data)
+            $state[$name] = ['flat'=>0, 'accum'=>0, 'move'=>0];
         
-
-        return view('summary',["summary" => $repository->findAll($summarized=true)]);
+        return view('summary',['state'=>$state, "start" => AgbarSimulator::getStartTime(), "end"=> AgbarSimulator::getEndTime(), "data" => $repository->findAll($summarized=true)]);
     }
 }
