0b65ce3fd0c3e9f78df51863ff55b3a7887dcfba
Xavi Subirà <xavi@proweb21.com>
2020-07-26 19:53:23 +0200

    REFACTOR applied in StateElevatorService [MODEL][SERVICES] StateElevatorService just fulfills the operation of stating an Elevator in a given an BuildingState, updating this last

--- src/Domain/Model/BuildingState/StateElevatorService.php
+++ src/Domain/Model/BuildingState/StateElevatorService.php
@@ -1,78 +1,25 @@
 <?php namespace Proweb21\Elevator\Model\BuildingState;
 
-use Proweb21\Elevator\Domain\ObserverTrait;
-use Proweb21\Elevator\Events\Observer;
-use Proweb21\Elevator\Model\ElevatorsStateFactory as StateFactory;
+use Proweb21\Elevator\Model\Building\Elevator;
 
 /**
  * Service responsible of stating elevators in a building
  * 
  */
-class StateElevatorsService
-    implements Observer
+final class StateElevatorsService
 {
-
-    use ObserverTrait;
-
     /**
-     * The Building Elevators state
+     * Service execution method
      *
-     * @var ElevatorsState
+     * @param Elevator $elevator
+     * @param BuildingState $state
+     * @return BuildingState
      */
-    protected $state;
-
-    /**
-     * The building with elevators
-     *
-     * @var Building
-     */
-    protected $building;
-
-
-    
-    /**
-     * Service constructor
-     *
-     * @param Building $building
-     * @param Factory $stateFactory
-     */
-    public function __construct(Building $building)
+    public function __invoke(Elevator $elevator, BuildingState $state)
     {
-        $this->building = $building;
-        $this->state = StateFactory::create($building);
-    
-        $this->observe(ElevatorFlatChanged::class, 'updateState');
-    }
+        $state->stateElevator($elevator);
 
-
-    /**
-     * Getter for the current service state
-     *
-     * @return FlatStateDTO[]
-     */
-    public function getState(): array
-    {
-        $result = [];
-
-        foreach($this->building->getFlats() as $flat_position => $flat){
-            $elevators = $this->state->getFlatState($flat_position);            
-            $result[$flat_position] = new FlatStateDTO($flat,$elevators);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Updates the state from an ElevatorFlatChanged event
-     *
-     * @param ElevatorFlatChanged $event
-     * @return void
-     */
-    public function updateState(ElevatorFlatChanged $event)
-    {
-        $flat = array_search($event->current_flat, $this->building->getFlats());
-
-        $this->state->setState($event->elevator_id, $flat);
+        return $state;
     }
 
 }
