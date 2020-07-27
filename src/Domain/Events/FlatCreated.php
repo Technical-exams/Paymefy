5f48c70517fce804441f6cf3884a16ef191fa5a6
Xavi Subirà <xavi@proweb21.com>
2020-07-27 15:27:32 +0200

    FlatCreated Event [DOMAIN][EVENT]

---
+++ src/Domain/Events/FlatCreated.php
@@ -0,0 +1,100 @@
+<?php namespace Proweb21\Elevator\Domain\Events;
+
+use Proweb21\Elevator\Events\EventTrait;
+use Proweb21\Elevator\Events\ObservableEvent;
+
+/**
+ * FlatCreated Domain Event
+ *
+ * This event occurs when an flat is created in a building
+ *
+ * @property-read \DateTimeImmutable $time
+ * @property-read string $name
+ * @property-read int $position
+ * @property-read string $building
+ */
+final class FlatCreated implements ObservableEvent
+{
+    use EventTrait;
+
+    /**
+     * Flat name
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Flat position
+     *
+     * @var int
+     */
+    protected $position;
+
+    /**
+     * Building's identifier
+     *
+     * @var string
+     */
+    protected $building;
+
+    public function __construct(string $name, int $position, string $building)
+    {
+        $this->name = $name;
+        $this->position = $position;
+        $this->building = $building;
+        $this->setTime();
+    }
+
+    /**
+     * Read-only properties accessor
+     *
+     * @param string $property
+     * @return void
+     */
+    public function __get($property)
+    {
+        switch ($property) {
+            case "building":
+                return $this->getBuilding();
+            break;
+            case "name":
+                return $this->getName();
+            break;
+            case "position":
+                return $this->getPosition();
+            break;
+        }
+    }
+
+
+    /**
+     * Getter for $building property
+     *
+     * @return string
+     */
+    public function getBuilding() : string
+    {
+        return $this->building;
+    }
+
+    /**
+     * Getter for $name property
+     *
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+ 
+    /**
+     * Getter for $position property
+     *
+     * @return integer
+     */
+    public function getPosition() : int
+    {
+        return $this->position;
+    }
+}
