470080a6c74e80bcd5c8da3a5390948b4dbb4464
Xavi Subirà <xavi@proweb21.com>
2020-07-27 09:05:05 +0200

    IMPROVED MoveElevatorService [SERVICE][DOMAIN] The service performs a more complex action: moves elevators from one flat to another, obeying user calls

--- src/Domain/Model/Building/MoveElevatorService.php
+++ src/Domain/Model/Building/MoveElevatorService.php
@@ -1,8 +1,10 @@
 <?php namespace Proweb21\Elevator\Model\Building;
 
 /**
- * Service responsible of moving elevators of a building
- * 
+ * Service responsible of moving elevators in a building
+ *
+ * Moves a chosen elevator from one flat to another, serving building elevators use
+ *
  */
 final class MoveElevatorService
 {
@@ -11,11 +13,20 @@ final class MoveElevatorService
      *
      * @param Building $building
      * @param Elevator $elevator
+     * @param Flat $from_flat
      * @param Flat $to_flat
      * @return Elevator
      */
-    public function __invoke(Building $building, Elevator $elevator, Flat $to_flat)
+    public function __invoke(Building $building, Elevator $elevator, Flat $from_flat, Flat $to_flat)
     {
-        return $building->moveElevator($elevator,$to_flat);
+        // Elevator has to move to the from flat
+        if (! Flat::equals($elevator->flat, $from_flat)) {
+            $building->moveElevator($elevator, $from_flat);
+        }
+        // Then has to be moved to the destination flat ($to_flat)
+        if (! Flat::equals($elevator->flat, $to_flat)) {
+            $building->moveElevator($elevator, $to_flat);
+        }
+        return $elevator;
     }
 }
