85e5e6b14da6cfdeccf5b29bd74710bc3742014d
Xavi Subirà <xavi@proweb21.com>
2020-07-28 17:34:38 +0200

    ElevatorsBus as Common Bus [INFRA][COMMON][COMMAND][BUS]

---
+++ src/Common/Infrastructure/Buses/ElevatorsBus.php
@@ -0,0 +1,22 @@
+<?php namespace Proweb21\Elevator\Infrastructure\Buses;
+
+use Proweb21\Bus;
+use Proweb21\CommandBus;
+
+/**
+ * Bus intended for elevator commands notification
+ *
+ * @package Common
+ */
+final class ElevatorsBus extends CommandBus implements Bus
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return string
+     */
+    public function name(): string
+    {
+        return 'elevators.bus';
+    }
+}
--- src/Common/Infrastructure/Buses/TimeBus.php
+++ src/Common/Infrastructure/Buses/TimeBus.php
@@ -2,17 +2,14 @@
 
 use Proweb21\Bus;
 use Proweb21\CommandBus;
-use Proweb21\DispatcherTrait;
 
 /**
  * Bus intended for time events notification
- * 
+ *
  * @package Common
  */
 final class TimeBus extends CommandBus implements Bus
 {
-    use DispatcherTrait;
-
     public function name(): string
     {
         return 'time.bus';
