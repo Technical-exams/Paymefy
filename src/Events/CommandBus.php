f415afae835c2b19eadb080b8ec44ff91ca0b645
Xavi Subirà <xavi@proweb21.com>
2020-07-28 12:01:26 +0200

    CommandBus implements Bus [COMMAND][BUS][APP][EVENTS][PUB-SUB]

--- src/Events/CommandBus.php
+++ src/Events/CommandBus.php
@@ -1,11 +1,8 @@
 <?php namespace Proweb21\Elevator\Events;
 
-use Proweb21\Elevator\Events\EventBus;
-use Proweb21\Elevator\Events\EventDispatcherTrait;
-
-final class CommandBus implements EventBus
+final class CommandBus implements Bus
 {
-    use EventDispatcherTrait;
+    use DispatcherTrait;
 
     public function name(): string
     {
