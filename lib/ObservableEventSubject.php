eea0a17ea3a7136b1e359cae71a25248fd6c9a5c
Xavi Subirà <xavi@proweb21.com>
2020-08-01 12:08:41 +0200

    RENAMED/improved DomainEventPublisher to DomainEvents [DOMAIN][EVENT][OBSERVER]

--- lib/ObservableEventSubject.php
+++ lib/ObservableEventSubject.php
@@ -1,7 +1,7 @@
 <?php namespace Proweb21;
 
 /**
- * Interface for implementations of helper classes notifying Observer of an Observable update 
+ * Interface for implementations of helper classes notifying Observer of an Observable update
  * These helper classes will be called directly from those Observable classes
  * @see https://en.wikipedia.org/wiki/Observer_pattern
  */
@@ -12,17 +12,17 @@ interface ObservableEventSubject
      * Attaches an observer observing (or listening) a kind of observable events
      *
      * @param Observer $observer An observer of observable events
-     * @param string $method Which method may be called in the observer
-     * @param string $observable_event_class
+     * @param string $observable class name of an observable
      * @return void
      */
-    public function attachObserver(Observer $observer, string $method, string $observable_event_class);
+    public function attachObserver(Observer $observer, string $observable);
 
     /**
-     * Notifies an ObservableEvent to all observers
+     * Notifies an ObservableEvent ocurred on Observable subject to all observers
      *
+     * @param Observable $subject
      * @param ObservableEvent $event
      * @return void
      */
-    public function updateObservers(ObservableEvent $event);
+    public function updateObservers(Observable $subject, ObservableEvent $event);
 }
---
+++ src/Common/Domain/DomainEvents.php
@@ -0,0 +1,99 @@
+<?php namespace Proweb21\Elevators\Common\Domain;
+
+use Proweb21\Observable;
+use Proweb21\ObservableEvent;
+use Proweb21\ObservableEventSubject;
+use Proweb21\Observer;
+
+/**
+ * Singleton class implementing the Observers Update notification system
+ * in the Observer pattern
+ * @see https://en.wikipedia.org/wiki/Observer_pattern
+ */
+final class DomainEvents implements ObservableEventSubject
+{
+    /**
+     * The domain event observers
+     *
+     * @var DomainEventObserver[]
+     */
+    private $observers;
+
+    /**
+     * The singleton instance of this class
+     *
+     * @var DomainEvents
+     */
+    protected static $instance;
+
+    /**
+     * Singleton class instance accessor
+     */
+    public static function instance() : DomainEvents
+    {
+        self::$instance = self::$instance ? : new static();
+        return self::$instance;
+    }
+
+    /**
+     * Protected constructor
+     * to protect the singleton
+     */
+    protected function __construct()
+    {
+        $this->observers = [];
+    }
+
+    /**
+     * Attaches an observer (akd Listener) to receive notifications
+     * of DomainEvents occurred in DomainSubjects
+     *
+     * @param DomainEventObserver $observer The observer to attach
+     * @param string $domain_subject The FQDN of the DomainSubject
+     *
+     * @return void
+     * 
+     * @throws \AssertionError when given observer is not an instance of DomainEventObserver
+     */        
+    public function attachObserver(Observer $observer, string $domain_subject)
+    {
+        // PRECONDITION
+        if ( !($observer instanceof DomainEventObserver) )
+            throw new \AssertionError("Observer is not a DomainEventObserver");
+
+        if (!array_key_exists($domain_subject, $this->observers)) {
+            $this->observers[$domain_subject] = [];
+        }
+        $this->observers[$domain_subject][] = $observer;
+    }
+
+    /**
+     * Notifies an DomainEvent to all observers of an DomainSubject
+     *
+     * @param DomainSubject $subject
+     * @param DomainEvent $event
+     * @return void
+     * 
+     * @throws \AssertionError When $subject is not a DomainSubject instance
+     *                         or when $event is not a DomainEvent instance
+     */
+    public function updateObservers(Observable $subject, ObservableEvent $event)
+    {
+        // PRECONDITION
+        if ( !($subject instanceof DomainSubject))
+            throw new \AssertionError("Cannot update observers of an object which is not a DomainSubject");
+        if ( !($subject instanceof DomainEvent))
+            throw new \AssertionError("Cannot update observers of an event which is not an DomainEvent");
+                
+        $class=get_class($subject);
+        if (!array_key_exists($class, $this->observers)) {
+            return;
+        }
+        foreach ($this->observers[$class] as $observer) {
+            $events_observed = $observer->getObservedEvents();
+            if (array_key_exists(get_class($event), $events_observed)) {
+                $observer->update($subject, $event);
+            }
+        }
+    }
+}
